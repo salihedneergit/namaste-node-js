c75d5147997ef2e017977adc0dee46ff15ff577b
salihedneergit <salihindiacp@gmail.com>
2024-12-22 22:07:38 +0530

    errorhandling

--- src/app.js
+++ src/app.js
@@ -1,15 +1,38 @@
 // middleware example to prevent unathorized access to webpage
 const express = require('express');
-const {auth} = require('../src/middleware/auth');
 const app = express();
-app.use('/admin',auth); 
-app.get('/admin/getUserData',(req,res)=>{
-    res.send("here is all the data : []");
+app.get('/getUserData',(req,res)=>{
+    // try{
+    //     throw new Error("Dvsss");
+    //     res.send("User data");
+    // }
+    // catch(error){
+    //     res.status(500).send("Error");
+    // }
+    throw new Error("Dv");
 })
-app.delete('/admin/deleteUser',(req,res)=>{
-    res.send("User is deleted");
+// orders matters a lot in express
+/*
+When an error is thrown, Express bypasses the normal route/middleware
+execution and looks for the next error-handling middleware in the stack. (same or parent domain only)
+*/
+app.get('/getUserData',(req,res)=>{
+    // try{
+    //     throw new Error("Dvsss");
+    //     res.send("User data");
+    // }
+    // catch(error){
+    //     res.status(500).send("Error");
+    // }
+    throw new Error("Dv");
 })
-
+app.use('/',(error,req,res,next)=>{
+    if(error){
+        // log your error
+        res.status(500).send("Something went wrong : 500");
+    }
+    res.send("hello");
+})
 app.listen(3000,()=>{
     console.log("Server started at 3000");
 })
